ee3d6034c031131c11b7d347ac798920945af8ff
Michele <ognioscar61@gmail.com>
2019-04-08 15:49:58 +0200

    Sostituzione file
    Sostituzione del file originale dovuto ad un errore ("Errore nella compilazione della scheda Arduino")

--- OgnibenMichele_A_CaccaPiGreco/OgnibenMichele_A_CaccaPiGreco.ino
+++ OgnibenMichele_A_CaccaPiGreco/OgnibenMichele_A_CaccaPiGreco.ino
@@ -26,11 +26,6 @@ void setup(){
   btn4     = 5;
   btn5     = 6;
   btnStart = 7;
-                                      //  variabili numeriche
-  punti    = 0;
-  tempo    = 1500;
-  vite     = 5;
-  punti    = 0;
                                       //  pinMode di input
   pinMode(btn1,     INPUT);
   pinMode(btn2,     INPUT);
@@ -53,12 +48,11 @@ void stampa(String f1, String f2){
 
 
 void stampaPiGreco(){
-  delay(2000);
+  delay(1500);
                                       //  Stampo il piGreco
   piGreco = (random(1, 6)* 3) - 2;
   lcd.setCursor(piGreco, 1);
   lcd.print("π");
-  delay(200);
   int premuto = 0;
   int i = 0;
   
@@ -88,15 +82,18 @@ void stampaPiGreco(){
 
 
 void loop() {
+  vite  = 5;
+  punti = 0;
+  tempo = 1500;
                                       //  btn inizio
   while(digitalRead(btnStart) == LOW){}
   lcd.clear();
-  stampa("5", "0");
-                                      //  con un random scelgo se far apparire bonus o mauls, in questo caso il 25% delle partite
+  stampa(String(vite), String(punti));
+                                      //  con un random scelgo se far apparire bonus o mauls, in questo caso il 15% circa delle partite
   while(vite >= 0){
-    if (random(1, 5) == 1){
-      stampaPiGreco();
-    }else if (random(1, 5) == 1){
+    if (random(1, 8) == 1){
+      stampaPiGreco();                //  con un random scelgo se far apparire bonus o mauls, in questo caso il 15% circa delle partite
+    }else if (random(1, 8) == 1){
       stampaPiGreco();
     }else{
       stampaPiGreco();
