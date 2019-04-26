caed65af98a830d8fa65b56b9704e8f686929881
Michele <ognioscar61@gmail.com>
2019-04-26 11:10:51 +0200

    Modifica del codice 24 - 04 - 19
    modifica del codice relativa al bonus ed il malus, rimozione bottone record con aggiunta del messaggio a fine partita del record

--- OgnibenMichele_A_CaccaPiGreco/OgnibenMichele_A_CaccaPiGreco.ino
+++ OgnibenMichele_A_CaccaPiGreco/OgnibenMichele_A_CaccaPiGreco.ino
@@ -7,7 +7,6 @@ int btn3;
 int btn4;
 int btn5;
 int btnStart;
-int btnRecord;
                                       //  variabili numeriche
 int vite;
 int punti;
@@ -24,7 +23,6 @@ void setup(){
   btn3      = 5;
   btn4      = 4;
   btn5      = 3;
-  btnRecord = 11;
   btnStart  = 12;
   record    = 0;
                                       //  pinMode di input
@@ -33,23 +31,18 @@ void setup(){
   pinMode(btn3,      INPUT);
   pinMode(btn4,      INPUT);
   pinMode(btn5,      INPUT);
-  pinMode(btnRecord, INPUT);
   pinMode(btnStart,  INPUT);
-  
-  lcd.print("Premi per");
-  lcd.setCursor(0, 1);
-  lcd.print("iniziare");
 }
 
 
-void stampa(String f1, String f2){
-  lcd.print("Vite:" + f1);
-  lcd.setCursor(16 - 6 - String(punti).length(), 0);
-  lcd.print("Punti:" + f2);
+void stampa(String vite, String punti){
+  lcd.print(vite);
+  lcd.setCursor(10 - String(punti).length(), 0);
+  lcd.print(punti);
 }
 
 
-void avvisa(String f1, String f2, String vite, String punti){
+void avvisa(String f1, String f2, int vite, int punti){
   lcd.clear();
   lcd.print(f1);
   lcd.setCursor(0, 1);
@@ -60,76 +53,86 @@ void avvisa(String f1, String f2, String vite, String punti){
 }
 
 
-void stampaPiGreco(int posizione, bool bonus, String s){
-      delay(1500);
-                                      //  Stampo il piGreco
-      int i = 0, premuto = 0, piGreco = (random(1, 6)* 3) - 2;
+int posizione(String s){
+  lcd.clear();
+  stampa(vite, punti);
+  delay(1500);
+                                  //  Stampo il piGreco     
+  int var = (random(1, 6)* 3) - 2;
+  lcd.setCursor(var, 1);
+  lcd.print(s);
+  return var;
+}
 
-      while(piGreco == posizione){piGreco = (random(1, 6)* 3) - 2;}
-      
-      lcd.setCursor(piGreco, 1);
-      lcd.print("π");
-      
-      lcd.setCursor(posizione, 1);
-      lcd.print(s);
-  
-      while  (i < tempo && premuto == 0){
-        if      (digitalRead(btn1) == HIGH){premuto = 1;}
-        else if (digitalRead(btn2) == HIGH){premuto = 4;}
-        else if (digitalRead(btn3) == HIGH){premuto = 7;}
-        else if (digitalRead(btn4) == HIGH){premuto = 10;}
-        else if (digitalRead(btn5) == HIGH){premuto = 13;}
-        delay(1);
-        i++;
-      }
-
-      if (premuto == posizione){
-        if (bonus == false){
-        punti--; 
-        tempo -= 50; 
-        vite--;
-        }else{
-          vite++;
-          tempo += 100;
-        }
-      }else if (premuto == piGreco){
-        punti++; 
-        if (tempo > 150)
-          tempo -= 50;
-      }else 
-        vite--;
-  
-      if (vite >= 0)
-        {lcd.clear(); stampa(String(vite), String(punti));}
+int premi(){
+  int i = 0, premuto = 0;
+                                  //  Ciclo che continua fino a che non finisce il tempo o l'utente preme un bottone
+  while  (i < tempo){
+    if      (digitalRead(btn1) == HIGH){premuto = 1;  break;}
+    else if (digitalRead(btn2) == HIGH){premuto = 4;  break;}
+    else if (digitalRead(btn3) == HIGH){premuto = 7;  break;}
+    else if (digitalRead(btn4) == HIGH){premuto = 10; break;}
+    else if (digitalRead(btn5) == HIGH){premuto = 13; break;}
+    delay(1);
+    i++;
+  }
+  return premuto;
 }
 
 
 void loop() {
   vite  = 5;
   punti = 0;
-  tempo = 1500;
+  tempo = 1200;
+  lcd.print("Premi per");
+  lcd.setCursor(0, 1);
+  lcd.print("iniziare");
                                       //  btn inizio
-  while(digitalRead(btnStart)  == LOW){
-    if (digitalRead(btnRecord) == HIGH){
-      lcd.clear();
-      lcd.print("Record: " + String(record) + "pt");
-      delay(300);
-      while (digitalRead(btnRecord) == LOW){}
-      lcd.clear();
-      stampa(String(vite), String(punti));
-      delay(300);
-    }
-  }
-  avvisa("Gioco iniziato", "preparati!", String(vite), String(punti));
-  while(vite >= 0){
-    if (random(1, 8) == 1){
-      stampaPiGreco((random(1, 6) * 3) - 2, true, "b");
-    }else if (random(1, 8) == 1){
-      stampaPiGreco((random(1, 6) * 3) - 2, false, "m");
+  while(digitalRead(btnStart)  == LOW){}
+  
+  avvisa("Gioco iniziato", "preparati!", vite, punti);
+  
+  while(vite > 0){                    //  bonus che esce con probabilità del 14% circa (1 volta su 8)
+    if (random(1, 9) == 1){
+      int var = posizione("b"), premuto = premi();
+      
+      if (var == premuto){
+        vite++; tempo += 50;
+      }
+      else{
+        vite--;
+      }
+    }else if (random(1, 9) == 1){     //  malus che esce con probabilità del 14% circa (1 volta su 8)
+      int var = posizione("m"), premuto = premi();
+      
+      if (var == premuto){
+        vite--; punti--; 
+        if (tempo > 250){tempo -= 40;}
+      }
+    }else{                            //  simbolo del pi greco
+      int var = posizione("p"), premuto = premi();
+      
+      if (var == premuto){
+        punti++;
+        if (tempo > 250){tempo -= 40;}
+      }
+      else{
+        vite--;
+      }
+    }                                 //  se l'utente non ha ancora finito le vite aggiorno vite e punti, altrimenti notifico che ha perso
+    if (vite > 0){
+    stampa(vite, punti);
     }else{
-      stampaPiGreco(-1, false, "");
+      avvisa("Hai perso!", "Riprova", 5, 0);
     }
+  }                                 //  aggiorno il record in caso sia stato appena fatto
+  lcd.clear();
+  if (punti > record){
+    record = punti;
+    lcd.print("Nuovo record!");
+    lcd.setCursor(0, 1);
   }
-  if (punti > record){record = punti;}
-  avvisa("Hai perso!", "Riprova", "5", "0");
+  lcd.print("Record: " + String(record));
+  delay(2500);
+  lcd.clear();
 }
